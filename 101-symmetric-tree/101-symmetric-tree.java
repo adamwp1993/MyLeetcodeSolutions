d747273e63017729dd2ae41b7f713f07a510a5d8
Adam Petersen <36431214+adamwp1993@users.noreply.github.com>
2022-05-12 10:19:55 -0400

    Time: 0 ms (100.00%), Space: 42.5 MB (31.99%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -16,17 +16,22 @@
 class Solution {
     
     
-    public boolean isMirror(TreeNode t1, TreeNode t2) {
-        if (t1 == null && t2 == null) return true;
-        if (t1 == null || t2 == null) return false;
-        return (t1.val == t2.val)
-            && isMirror(t1.right, t2.left)
-            && isMirror(t1.left, t2.right);
-}
-    
     // Recursive
     public boolean isSymmetric(TreeNode root) {
+       
         return isMirror(root, root);
             
         }
-    }
+    
+    
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null) { return true; }
+        if(t1 == null || t2 == null) { return false; }
+
+
+        return (t1.val == t2.val) 
+            && isMirror(t1.right, t2.left) 
+            && isMirror(t1.left , t2.right);  
+        
+        }
+}
