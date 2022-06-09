c30d5151c95a457d65be20ccb6ccc2b9c2ad6593
Adam Petersen <36431214+adamwp1993@users.noreply.github.com>
2022-06-09 10:49:34 -0400

    Time: 5 ms (10.30%), Space: 84.2 MB (20.65%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,23 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
         
-        int bestProfit = 0;
-        int minPrice = Integer.MAX_VALUE;
+        // Kadenes
+        // O(N)
+        int currMax = 0;
+        int globalMax = 0;
         
-        for(int i = 0; i < prices.length; i++) {
-            
-            if(prices[i] < minPrice) {
-                minPrice = prices[i];
-            }
-            
-            else if(prices[i] - minPrice > bestProfit) {
-                bestProfit = prices[i] - minPrice;
-            }
-            
+        for(int i = 1; i < prices.length; i++) {
+            // determine if previous subarrays difference plus this values difference is greater or less 0.
+            // if less than we reset to 0. 
+            // if the difference is greater than anything we've seen previously, update global. 
+            currMax = Math.max(0, currMax += prices[i] - prices[i-1]);
+            if(currMax > globalMax) {
+                globalMax = currMax;
+            }                                                                                                                                                                                                                                       
         }
-        return bestProfit;
-        
+        return globalMax;
+    
     }
-    
-    
 }
