1252615b1f425d9e8d53cc5cf104f4edbe5771e5
Adam Petersen <36431214+adamwp1993@users.noreply.github.com>
2022-06-09 23:02:09 -0400

    Time: 2 ms (56.26%), Space: 44.9 MB (43.06%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -2,13 +2,13 @@ class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         // Sliding window Pattern 
         int smallestLength = Integer.MAX_VALUE;
-        
+        int len = nums.length;
         // create range 
         int left = 0;
         int right = 0;
         int sum = 0;
         // expand subarray until condition is met 
-        while(right < nums.length) {
+        while(right < len) {
             sum += nums[right];
             right++;
             // once condition is met, contract until it is not
@@ -19,7 +19,6 @@ class Solution {
                 smallestLength = Math.min(smallestLength, right - left + 1);
             }
         }
-        if(smallestLength == Integer.MAX_VALUE) return 0;
-        else return smallestLength;
+        return smallestLength == Integer.MAX_VALUE ? 0 : smallestLength;
     }
 }
