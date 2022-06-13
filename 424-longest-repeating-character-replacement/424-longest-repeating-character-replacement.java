d1c85d46837fb1410a450e66ec3b20591afae61d
Adam Petersen <36431214+adamwp1993@users.noreply.github.com>
2022-06-13 13:31:35 -0400

    Time: 9 ms (67.16%), Space: 43.7 MB (36.49%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -2,46 +2,24 @@ class Solution {
     public int characterReplacement(String s, int k) {
         // init range and data structures 
         // HashMap key = character and Value = # of occurences in subarray 
-        int left = 0, right = 0, longest = 0;
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        char[] vals = s.toCharArray();
+        int left = 0, longest = 0, mostFreq = 0;
         
-        // expand window, updating counters along the way, until subarray is not valid. 
-        for(; right < vals.length; right++) {
-            // add to hashMap 
-            if(map.containsKey(vals[right])) {
-                map.put(vals[right], map.get(vals[right]) + 1);
-            }
-            else {
-                map.put(vals[right], 1);
-            }
-            // determine String w/ most occurences and how many occurences it had
-            int highest = 0;
-            Character mostOccur;
-            for(Character key : map.keySet()) {
-                if(highest < map.get(key)) {
-                    mostOccur = key;
-                    highest = map.get(key);
-                }
-            }
-            // if valid subarray update longest subarray encountered 
-            if((right - left + 1) - highest <= k) {
-                longest = Math.max(longest, right - left + 1);
-            }
-            // while subArray is not valid, contract window 
-            while((right - left + 1) - highest > k && left <= right) {
-                // decrement val, move left pointer 
-                map.put(vals[left], map.get(vals[left]) - 1);
+        
+        int[] counters = new int[26];
+        
+        // expand window
+        for(int right = 0; right < s.length(); right++) {
+            // update counter 
+            counters[s.charAt(right) - 'A']++;
+            // Determine most frequent character 
+            mostFreq = Math.max(mostFreq, counters[s.charAt(right) - 'A']);
+            // find out how many we have to change 
+            int change = (right - left + 1) - mostFreq;
+            if(change > k) {
+                counters[s.charAt(left) - 'A']--;
                 left++;
-                // re-calculate most occured 
-                highest = 0;
-                for(Character key: map.keySet()) {
-                    if(highest < map.get(key)) {
-                        mostOccur = key;
-                        highest = map.get(key);
-                    }
-                }
             }
+            longest = Math.max(longest, right - left + 1);
         }
         return longest;
         
