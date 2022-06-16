fd05c2b1a9e06494953e2e41a27d4fd4f3406cc4
Adam Petersen <36431214+adamwp1993@users.noreply.github.com>
2022-06-15 21:35:32 -0400

    Time: 1046 ms (6.06%), Space: 47.7 MB (55.63%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        // sliding window 
+        
+        int left = 0;
+        List<Integer> result = new ArrayList<Integer>();
+        // track vals w/ arrays 
+        int[] scounter = new int[26];
+        int[] pcounter = new int[26];
+        
+        if(s.length() < p.length()) return result;
+            
+        // count string p 
+        for(int i = 0; i < p.length(); i++) {
+            ++pcounter[p.charAt(i) - 'a'];
+        }
+        
+        // window 
+        for(int right = 0; right < s.length(); right++) {
+            ++scounter[s.charAt(right) - 'a'];
+            // check if window is valid 
+            boolean valid = true;
+            for(int j = 0; j < p.length(); j++) {
+                if(pcounter[p.charAt(j) - 'a'] != scounter[p.charAt(j) - 'a']) {
+                    valid = false;
+                    break;
+                }
+            }
+            if(valid) {
+                result.add(left);
+            }
+            // slide window right 
+            if(right - left + 1 == p.length()) {
+                --scounter[s.charAt(left) - 'a'];
+                left++;
+            }
+        }
+        return result;
+        
+    }
+}
