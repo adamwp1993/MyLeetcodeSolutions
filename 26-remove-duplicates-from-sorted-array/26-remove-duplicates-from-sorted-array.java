7b28ee0530c697062b6e5f44411ea50b13c05877
Adam Petersen <36431214+adamwp1993@users.noreply.github.com>
2022-06-25 11:32:37 -0400

    Time: 5 ms (11.46%), Space: 48.7 MB (6.97%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,22 +1,19 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        int k = 0;
         int n = nums.length;
-        int k = 0; 
         HashSet<Integer> seen = new HashSet<Integer>();
         
-        // replace dupes with a value we know to be outside the range 
         for(int i = 0; i < n; i++) {
             if(seen.contains(nums[i])) {
-                nums[i] = 1000;
+                k++;
             }
             else {
                 seen.add(nums[i]);
-                k++;
             }
+            nums[i - k] = nums[i];
         }
-        // then we just sort!
-        Arrays.sort(nums);
-        return k;
+        return n - k;
         
     }
 }
