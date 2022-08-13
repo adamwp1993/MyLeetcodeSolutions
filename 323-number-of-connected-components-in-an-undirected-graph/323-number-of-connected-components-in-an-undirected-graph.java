daaf2aa55c90d6a74ccf4dcf12108b726ba31a07
Adam Petersen <36431214+adamwp1993@users.noreply.github.com>
2022-08-13 14:22:25 -0400

    Time: 13 ms (22.69%), Space: 49.7 MB (17.33%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,55 +1,39 @@
 class Solution {
-    int rank[];
-    int root[];
-    // DISJOINT SET FUNCTIONS 
-    public void buildSet(int size) {
-        rank = new int[size];
-        root = new int[size];
-        for(int i = 0; i < size; i++) {
-            rank[i] = 1;
-            root[i] = i;
-        }
-    }
-    
-    public int find(int x) {
-        if(x == root[x]) {
-            return x;
-        }
-        return root[x] = find(root[x]);
-    }
-    
-    public void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if(rootX != rootY) {
-            if(rank[rootX] > rank[rootY]) {
-                root[rootY] = rootX;
-            }
-            else if(rank[rootY] > rank[rootX]) {
-                root[rootX] = rootY;
-            }
-            else {
-                root[rootY] = rootX;
-                rank[rootX] += 1;
-            }
-        }
-    }
     
     public int countComponents(int n, int[][] edges) {
-        // 1. build set 
-        // 2. populate unions 
-        // 3. count roots. number of roots = # of components 
-        int count = 0;
-        // build set
-        buildSet(n);
-        // populate union
-        for(int i = 0; i < edges.length; i++) {
-            union(edges[i][0], edges[i][1]);
+        // create graph / adjacency list 
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            graph.put(i, new ArrayList<>());
         }
-        // count roots
-        for(int j = 0; j < n; j++) {
-            if(j == find(j)) count++;
+        for(int i = 0; i < edges.length; i++) {
+            int nodea = edges[i][0];
+            int nodeb = edges[i][1];
+            graph.get(nodea).add(nodeb);
+            graph.get(nodeb).add(nodea);
+        }
+        
+        boolean[] visited = new boolean[n];
+        
+        // run DFS from each node until all have been visited
+        int count = 0;
+        for(int p = 0; p < n; p++) {
+            count += dfs(p, graph, visited);
         }
         return count;
+        
     }
+    
+    public int dfs(int node, HashMap<Integer, List<Integer>> graph, boolean[] visited) {
+        // if already visited return 0
+        if(visited[node]) return 0;
+        // mark node as visited
+        visited[node] = true;
+        // visit neighbor nodes recursively
+        for(int neighbor : graph.get(node)) {
+            dfs(neighbor, graph, visited);
+        }
+        return 1;
+    }
+        
 }
