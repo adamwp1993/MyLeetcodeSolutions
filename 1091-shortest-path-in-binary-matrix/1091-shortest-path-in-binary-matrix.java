0308d6dd03519bf076555ec78990146de98f5675
Adam Petersen <36431214+adamwp1993@users.noreply.github.com>
2022-09-01 07:20:58 -0400

    Time: 36 ms (36.83%), Space: 61.2 MB (35.41%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,64 +1,51 @@
 class Solution {
-    public int shortestPathBinaryMatrix(int[][] grid) {
-        
-        if(grid[0][0] == 1) return -1;
-        
-        int n = grid.length;
-        int m = grid[0].length;
-        boolean[][] visited = new boolean[n][m];
-        Queue<int[]> q = new ArrayDeque<>();;
-        q.add(new int[]{0, 0, 1});
-        visited[0][0] = true;
-        
-        // start BFS 
-        while(!q.isEmpty()) {
-            int[] curNode = q.remove();
-            int row = curNode[0];
-            int col = curNode[1];
-            int distance = curNode[2];
-            // if the current node is the bottom right corner, return the distance 
-            if(row == n - 1 && col == m - 1) return distance;
-            // mark visited 
-            visited[row][col] = true;
-            
-            // get all neighbors 
-            List<int[]> neighbors = getNeighbors(row, col, grid);
-            // add valid neighbors to the queue 
-            for(int[] neighbor : neighbors) {
-                int nRow = neighbor[0];
-                int nCol = neighbor[1];
-                if(visited[nRow][nCol]) {
-                    continue;
-                }
-                visited[nRow][nCol] = true;
-                q.add(new int[] {nRow, nCol, distance + 1});
-            }
-            // mark visited 
-            visited[row][col] = true;
-            
-            
-        }
-        
-        return -1;
-    }
+    private static final int[][] directions = new int[][] {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
     
-    public List<int[]> getNeighbors(int row, int col, int[][] grid ) {
-        // returns a 2d list of all valid, unvisited neighbor nodes 
-        // 8 directions 
-        int[][] directions = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1},{1, -1}, {1, 0}, {1, 1}};
-        List<int[]> neighbors = new ArrayList<>();
-        
-        for(int i = 0; i < directions.length; i++) {
-            int newRow = row + directions[i][0];
-            int newCol = col + directions[i][1];
-            // validate the cell position, make sure indices are within bounds, its a cell with 0 and hasnt been visited 
-            if(newRow < 0 || newCol < 0 || newRow >= grid.length || newCol >= grid[0].length || grid[newRow][newCol] != 0) {
-                continue;
-            }
-            neighbors.add(new int[]{newRow, newCol});
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        // n x n grid
+        int n = grid.length;
+        // prepare data structures for BFS
+        boolean[][] visited = new boolean[n][n];
+        Queue<int[]> q = new LinkedList<>();
+        // add top left corner to the queue if its a 0. if not, we cant reach it, so return -1
+        if(grid[0][0] == 0) {
+            q.offer(new int[] {0, 0, 1});
+        }
+        else {
+            return -1;
         }
         
-        return neighbors;
+        // run dfs 
+        while(!q.isEmpty()) {
+            for(int i = q.size(); i > 0; i--) {
+                int[] cell = q.poll();
+                int row = cell[0];
+                int col = cell[1];
+                int distance = cell[2];
+                // condition met - bottom right cell reached, return distance 
+                if(row == n - 1 && col == n - 1) return distance;
+
+                // mark visited if not already 
+                visited[row][col] = true;
+                // evaluate neighbors 
+                for(int[] dir : directions) {
+                    int newRow = row + dir[0];
+                    int newCol = col + dir[1];
+                    // validate the indices, check that its a traversable cell (a 0), and that its not prevously been visited. 
+                    if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < n && !visited[newRow][newCol] && grid[newRow][newCol] == 0 ) {
+
+                        q.offer(new int[] {newRow, newCol, distance + 1});
+                        // this is because the same cell is getting added to array multiple times causing TLE. so a cell may be added multiple times before
+                        // we get a chance to evaluate it and set visited to true, which wastes time and space in our queue.
+                        visited[newRow][newCol] = true;
+                        
+                    }
+                }
+            }
+                   
+        }
+        // if we make it here, that means we did not find a path. return -1
+        return -1;
         
         
         
