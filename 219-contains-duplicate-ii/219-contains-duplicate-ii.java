4a7454480f4d219d43b801fc37060b2750b9c58e
Adam Petersen <36431214+adamwp1993@users.noreply.github.com>
2022-09-18 12:16:42 -0400

    Time: 57 ms (25.93%), Space: 78.9 MB (67.54%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // Hash Table holds the most recent index of an element 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // Hash Table holds all values withing a "window"
+        HashSet<Integer> set = new HashSet<>(nums.length);
         
         for(int i = 0; i < nums.length; i++) {
-            // if we find a duplicate, evaluate if it meets the criteria
-            if(map.containsKey(nums[i])) {
-                // determine if absolute value of i - j <= k
-                int j = map.get(nums[i]);
-                if(Math.abs(i - j) <= k) {
-                    return true;
-                }
+            if(set.contains(nums[i])) {
+                return true;
             }
-            // else we update the most recent index of this value. 
-            map.put(nums[i], i);
+            set.add(nums[i]);
+            if(set.size() > k) {
+                set.remove(nums[i - k]);
+            }
         }
-        // return false if no valid condition existed 
         return false;
+        
     }
 }
